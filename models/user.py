f0682db53258901214d39625a0d1c4eb8fd041d4
oreste-abizera <oresteabizera11@gmail.com>
2024-09-27 00:48:16 +0200

    fix: User inherits Base

--- models/user.py
+++ models/user.py
@@ -5,7 +5,7 @@ from sqlalchemy import String, DateTime, Column, ForeignKey
 from sqlalchemy.orm import relationship
 
 
-class User(BaseModel):
+class User(BaseModel, Base):
     """This class defines a user by various attributes"""
     __tablename__ = 'users'
 
