d774e361f3186f781c0f64ea1bb69221082806d8
oreste-abizera <oresteabizera11@gmail.com>
2024-10-12 18:32:07 +0200

    chore: improve engines

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -64,3 +64,7 @@ class FileStorage:
             self.save()
         else:
             pass
+
+    def close(self):
+        """call reload() method for deserializing the JSON file to objects"""
+        self.reload()
